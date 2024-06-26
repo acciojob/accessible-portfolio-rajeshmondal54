b382fad67509499f71e52bda8049d1af555a1a34
rajeshmondal54 <rm4223536@gmail.com>
2024-06-26 15:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+describe('Accessibility Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should have correct ARIA roles', () => {
+    cy.get('h1').should('have.attr', 'role', 'heading');
+
+    cy.get('nav').should('have.attr', 'role', 'navigation');
+    cy.get('nav ul').should('have.attr', 'role', 'list');
+
+    cy.get('li').each(($li) => {
+      cy.wrap($li).should('have.attr', 'role', 'listitem');
+    });
+
+    cy.get('img').should('have.attr', 'role', 'img');
+  });
+});
